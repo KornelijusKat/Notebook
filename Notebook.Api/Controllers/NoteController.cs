a36668ea73a89205491879a1fff0e3b9f371e67a
KornelijusKat <katinaskornelijus@gmail.com>
2022-07-07 19:24:30 +0300

    First Commit

---
+++ Notebook.Api/Controllers/NoteController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Notebook.Authentication.NewFolder;
+using Notebook.Database;
+using Notebook.Database.DatabaseService;
+using Notebook.Database.ObjectDto;
+using NoteBook.ImageService.NewFolder;
+using NoteBook.ImageService.Validations;
+using System;
+
+namespace Notebook.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NoteController : Controller
+    {
+        private readonly IDbService _dbContext;
+        private readonly IServiceImage _serviceImage;
+        private readonly IJwtService _jwtService;
+        private readonly IAccount _account;
+        public NoteController(IDbService dbContext, IServiceImage serviceImage, IJwtService jwtService, IAccount account)
+        {
+            _dbContext = dbContext;
+            _serviceImage = serviceImage;
+            _jwtService = jwtService;
+            _account = account;
+        }
+        [HttpGet("Log in")]
+        public IActionResult LogIn([FromQuery] string name, [FromQuery] string lastname)
+        {
+            var user = _account.Login(name, lastname);
+            if (user)
+            {
+                var token = _jwtService.GetJwtToken(name);               
+                return Ok(new { Token = token });
+            }
+            return NotFound("Username or password was wrong");
+        }
+        [HttpPost("Create User")]
+        public IActionResult Index([FromQuery] UserDto newUser)
+        {
+            _account.SignUpAccount(newUser.FirstName,newUser.LastName);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("Create Record")]
+        public IActionResult CreateRecord([FromQuery]RecordDto record,[FromForm] ValidatingExtension imageRequest)
+        {
+            byte[] imageBytes = _serviceImage.ConvertImageToBytes(imageRequest);
+            _dbContext.AddRecord(record,imageBytes);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost("Create Category")]
+        public IActionResult CreateCategory([FromQuery] string name,[FromQuery] Guid userid)
+        {
+            _dbContext.AddCategory(name,userid);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("Change Record")]
+        public IActionResult ChangeRecord([FromQuery] RecordDto newData,[FromQuery] string id)
+        {
+            _dbContext.ChangeRecordCategory(Guid.Parse(id), newData);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPut("Change Category information")]
+        public IActionResult ChangeInformation([FromQuery] Guid id, [FromQuery] string newName)
+        {
+            _dbContext.EditCategory(id, newName);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("ShowCategoryRecords")]
+        public IActionResult ShowRecords([FromQuery] Guid id)
+        {
+            var categoryRecords = _dbContext.ShowRecordsByCategory(id);
+            return Ok(categoryRecords);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpGet("SearchRecordById")]
+        public IActionResult ShowRecord([FromQuery] Guid id)
+        {
+            var record = _dbContext.SearchRecordByID(id);
+            return Ok(record);
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpDelete("Delete Record")]
+        public IActionResult DeleteRecord([FromQuery] Guid id)
+        {
+            _dbContext.RemoveRecord(id);
+            return Ok();
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpDelete("Delete Category")]
+        public IActionResult DeleteCategory([FromQuery] Guid Id)
+        {
+            _dbContext.DeleteCategory(Id);
+            return Ok();
+        }
+    }
+}
---
+++ Notebook.Api/Startup.cs
@@ -0,0 +1,107 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Notebook.Authentication;
+using Notebook.Database.Extensions;
+using NoteBook.ImageService.Extensions;
+using System;
+using System.Text;
+
+
+namespace Notebook.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddSession(options => {
+                options.IdleTimeout = TimeSpan.FromHours(1);
+            });
+            services.AddImageService();
+            services.AddDatabase();
+            services.AddAuth();
+            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Secret"]))
+                };
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Notebook.Api", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme()
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[] { }
+                    }
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Notebook.Api v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseCors(x => x
+       .AllowAnyMethod()
+       .AllowAnyHeader()
+       .SetIsOriginAllowed(origin => true)
+       .AllowCredentials());
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseSession();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
